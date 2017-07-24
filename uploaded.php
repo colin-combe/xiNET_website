3190c58dd5965b2c2a3015a37f4b9c099a39b8bb
Colin Combe <colin.combe@ed.ac.uk>
2017-07-24 18:20:34 +0100

    fix

--- uploaded.php
+++ uploaded.php
@@ -37,14 +37,15 @@
         <link rel="stylesheet" href="./css/noNav.css" />
         <link rel="stylesheet" href="./css/xiNET.css" />
         <!--libraries-->
+<!--
         <script type="text/javascript" src="../xiNET/vendor/d3.js"></script>
         <script type="text/javascript" src="../xiNET/vendor/colorbrewer.js"></script>
         <script type="text/javascript" src="../xiNET/vendor/FileSaver.js"></script>
         <script type="text/javascript" src="../xiNET/vendor/rgbcolor.js"></script>
-        <!--xiNET-->
-<!--
-        <script type="text/javascript" src="../xiNET/crosslinkviewer.js"></script>
 -->
+        <!--xiNET-->
+        <script type="text/javascript" src="../xiNET/crosslinkviewer.js"></script>
+<!--
         <script type="text/javascript" src="../xiNET/src/controller/Init.js"></script>
         <script type="text/javascript" src="../xiNET/src/controller/MouseEvents.js"></script>
         <script type="text/javascript" src="../xiNET/src/controller/Layout.js"></script>
@@ -61,6 +62,7 @@
         <script type="text/javascript" src="../xiNET/src/controller/xiNET_Storage.js"></script>
         <script type="text/javascript" src="../xiNET/src/controller/ReadCSV.js"></script>
         <script type="text/javascript" src="../xiNET/src/controller/Fasta.js"></script>
+-->
     </head>
     <body>
 
