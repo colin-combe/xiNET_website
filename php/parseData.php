adb0908ee849224c88c98ba5d8df677d4212e514
colin-combe <colin.combe@ed.ac.uk>
2018-05-23 07:19:24 +0100

    wip

--- php/parseData.php
+++ php/parseData.php
@@ -50,35 +50,7 @@
 		die('error: invalid post data!');
 	}
 
-	$date = date('Y-m-d H:i:s');
-	$ip = getUserIP();
-	$ipInfo = json_decode(file_get_contents("https://ipinfo.io/{$ip}/"));
-	$hostname = (property_exists($ipInfo, 'hostname')) ? $ipInfo->hostname : '' ;
-	$country = (property_exists($ipInfo, 'country')) ? $ipInfo->country : '' ;
-	$region = (property_exists($ipInfo, 'region')) ? $ipInfo->region : '' ;
-	$city = (property_exists($ipInfo, 'city')) ? $ipInfo->city : '' ;
-	$org = (property_exists($ipInfo, 'org')) ? $ipInfo->org : '' ;
-
 	$xiSPEC_ms_parser_dir = '../../xiSPEC_ms_parser/';
-
-	#this includes a connection string to the sql database
-	require('../xiSPEC_sql_conn.php');
-	$xiSPECdb = new PDO("mysql:host=localhost;dbname=".$DBname, $DBuser, $DBpass) or die("cannot open the database");
-	$xiSPECdb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-	$stmt = $xiSPECdb->prepare("INSERT INTO `upload_log`(`id_file`, `pl_file`, `ip`, `hostname`, `country`, `region`, `city`, `org`, `date`)
-															VALUES (:id_file, :pl_file, :ip, :hostname, :country, :region, :city, :org, :dates);");
-	$stmt->bindParam(':id_file', $id_file, PDO::PARAM_STR);
-	$stmt->bindParam(':pl_file', $pl_file, PDO::PARAM_STR);
-	$stmt->bindParam(':ip', $ip, PDO::PARAM_STR);
-	$stmt->bindParam(':hostname', $ipInfo->hostname, PDO::PARAM_STR);
-	$stmt->bindParam(':country', $ipInfo->country, PDO::PARAM_STR);
-	$stmt->bindParam(':region', $ipInfo->region, PDO::PARAM_STR);
-	$stmt->bindParam(':city', $ipInfo->city, PDO::PARAM_STR);
-	$stmt->bindParam(':org', $ipInfo->org, PDO::PARAM_STR);
-	$stmt->bindParam(':dates', $date, PDO::PARAM_STR);
-
-	$stmt->execute();
 
 	$argStr = implode(' ', [$ftp_arg, $id_arg, $pl_arg, $upload_arg]);
 
