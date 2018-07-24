8069368ec80347886ae7ed646a63e0b0c67d345b
Colin <colin.combe@ed.ac.uk>
2018-07-24 14:30:58 +0100

    confirmation reminder

---
+++ confirmationReminder.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <?php
+        $pageName = "Privacy Statement";
+        include("head.php");
+        ?>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+
+    </head>
+
+    <body>
+    <!-- Sidebar -->
+    <?php include("navigation.php");?>
+
+    <!-- Main -->
+    <div id="main">
+        <div class="container">
+            <h1 class="page-header">Email confirmation required</h1>
+
+				<p>An email was sent to the address you used to register for xiVIEW.
+                    <br>You must click the link contained in that email before you can sign in. 
+                    <br>By doing so, you are giving us permission to store your data in accordance with our <a href="./privacy.php"> Privacy Statment</a>.
+                    <br>The privacy statement is also contained in the email you recieved</p>
+                </p>
+
+        </div> <!-- CONTAINER -->
+    </div> <!-- MAIN -->
+
+    </body>
+</html>
--- privacy.php
+++ privacy.php
@@ -24,7 +24,8 @@
 				<p>xiView requires creating a user account; this is used to provide an index page to the data you have uploaded. This index page allows you to aggregate and/or compare your datasets.
 				    <br>We store your email address, which will only be used for password reset requests and the initial account set-up confirmation.
 					<br>Your uploaded data is kept private; we will not make use of your data or provide access to others.
-					<br>We will use the total number of user accounts and total number of datasets uploaded when we report usage statistics.
+					<br>We will use the number of user accounts and number of datasets uploaded when we report usage statistics.
+                    <br>Cookies are used to record that you have logged in, and, optionally, to store the configuration of the filters in the 'My Data' page.       
 					<br>This is a beta/development version of this website and logging is currently enabled on the web server, we will only use this for debugging purposes.
                     <br>You will recieve an email asking you to agree to these uses of your data when you create your account.
                 </p>
