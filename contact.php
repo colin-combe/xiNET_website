6a2473f0721f65fbe61c7bfd5eafb50495834fa6
Colin Combe <colin.combe@ed.ac.uk>
2025-07-18 15:55:28 +0200

    update contact page

--- contact.php
+++ contact.php
@@ -16,41 +16,30 @@
 				<!-- Intro -->
 					<section id="top" class="one contact-section">
 						<div class="container">
-							<div class="page-header">Contact The Team</div>
+							<div class="page-header">This website is no longer maintained</div>
 							<div class="contactGroup">
 								<table>
 									<tr>
 						   		 <td><div class="contact no-hover">
 						   		 		<ul class="navlist">
-						   		 			<li class="no-hover"><a class="colin no-hover" href="mailto:colin.combe@ed.ac.uk" title="Email Colin!"></a></li>
+						   		 			<li class="no-hover"><a></a></li>
 						   		 		</ul>
 									<div class="external-link">
 						   		 	<p class="colintext">
 						   		 		<strong>Dr Colin Combe</strong>
+						   		 		<br>Developer
 						   		 		<br/><a href="mailto:colin.combe@ed.ac.uk">colin.combe@ed.ac.uk</a>
-						   		 		<br/>Developer
-						   		 		<br/>Room 4.17
-						   		 		<br/>Michael Swann Building
-						   		 		<br/>King’s Buildings
-						   		 		<br/>Edinburgh
-						   		 		<br/>EH9 3JR
 						   		 	</p>
 									</div>
 						   		 </div> <!-- CONTACT--></td>
 			               		<td> <div class="contact">
 						   		 		<ul class="no-hover navlist">
-						   		 			<li><a class="matt no-hover" href="mailto:matthew.dunn@ed.ac.uk" title="Email Matt!"></a></li>
+						   		 			<li></li>
 						   		 		</ul>
 						   		 	<div class="external-link">
 						   		 	<p class="matttext">
 						   		 		<strong>Matthew Dunn</strong>
-						   		 		<br/><a href="mailto:matthew.dunn@ed.ac.uk">matthew.dunn@ed.ac.uk</a>
 						   		 		<br/>Web Designer
-						   		 		<br/>Room 4.15
-						   		 		<br/>Michael Swann Building
-						   		 		<br/>King’s Buildings
-						   		 		<br/>Edinburgh
-						   		 		<br/>EH9 3JR
 						   		 	</p>
 									</div>
 						   		 </div> <!-- CONTACT -->
@@ -64,13 +53,8 @@
 									<div class="external-link">
 						   		 	<p class="juritext">
 						   		 		<strong>Prof Juri Rappsilber</strong>
-						   		 		<br/><a  href="mailto:juri.rappsilber@ed.ac.uk">juri.rappsilber@ed.ac.uk</a>
+						   		 		<br/><a  href="mailto:juri.rappsilber@tu-berlin.de">juri.rappsilber@tu-berlin.de</a>
 						   		 		<br/>General Inquiries
-						   		 		<br/>Room 4.18a
-						   		 		<br/>Michael Swann Building
-						   		 		<br/>King’s Buildings
-						   		 		<br/>Edinburgh
-						   		 		<br/>EH9 3JR
 						   		 	</p>
 									</div>
 						   		 </div> <!-- CONTACT --></td>
@@ -79,16 +63,6 @@
 							</div><!-- CONTACTGROUP -->
 							<br>
 						</div> <!-- CONTAINER -->
-					</section>
-					<section class="two contact-section2">
-						<div class="container">
-							<div class="external-link">
-								<h4>Support</h4>
-								<p>xiNET can be used by <a href="./upload.php">uploading your data</a> or downloading the <a target="_blank" href="https://github.com/colin-combe/crosslink-viewer/releases/latest"><i class="fa fa-github"></i>&nbsp;GitHub project</a> and using it locally. 
-								<br> For support email <a href="mailto:colin.combe@ed.ac.uk">colin.combe@ed.ac.uk</a>. You may also use <a target="_blank" href="https://github.com/colin-combe/crosslink-viewer/issues"> issue tracking at GitHub</a>.</p>
-							</div>
-							<br>
-						</div>
 					</section>
 			</div> <!-- MAIN -->
 	</body>
